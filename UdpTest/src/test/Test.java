faf6c3a2c139d3a1b87c522330cfc0db9443d625
YUIL <290945322@qq.com>
2015-03-10 18:31:08 +0800

    changed by andoird

--- UdpTest/src/test/Test.java
+++ UdpTest/src/test/Test.java
@@ -1,11 +1,7 @@
 package test;
-import util.JavaDataConverter;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketAddress;
+import java.io.*;
+import java.net.*;
+import util.*;
 
 public class Test {
 	public static void main(String args[]){
@@ -16,21 +12,28 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		byte temp=1;
 		while(true){
 			byte[] buf=new byte[4];
-			DatagramPacket datagramPacket =new DatagramPacket(buf,4);
+			buf[0]=temp;
+
 			try {
-				serverSocket.receive(datagramPacket);
+				DatagramPacket datagramPacket =new DatagramPacket(buf,4,InetAddress.getByName("192.168.100.33"),9091);
+				serverSocket.send(datagramPacket);
+
+
+				System.out.println(JavaDataConverter.bytesToInt(datagramPacket.getData()));
+
+				if(JavaDataConverter.bytesToInt(datagramPacket.getData())==255){
+					break;
+				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println(JavaDataConverter.bytesToInt(datagramPacket.getData()));
-			if(JavaDataConverter.bytesToInt(datagramPacket.getData())==0){
-				break;
-			}
+			temp++;
+
 		}
-
+		System.out.println("app end");
 	}
-
 }
